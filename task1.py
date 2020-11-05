83d224f27712d4e8b5d6f7bce961d636f52983f1
LeoZhang553 <leoz553@deltalearns.ca>
2020-11-05 12:07:07 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -8,7 +8,7 @@ win.title("Trangle Area Calculator")
 win.geometry("600x200")
 
 eoutput = StringVar()
-eoutput.set("Output goes here")
+eoutput.set("insufficient conditoins")
 
 l1=tk.Label(win,text=" Enter side 1: ")
 l2=tk.Label(win,text=" Enter side 2: ")
@@ -26,15 +26,7 @@ e3=tk.Entry(win,width=10)
 e4=tk.Entry(win,width=10)
 e5=tk.Entry(win,width=10)
 
-e5=tk.Entry(win,width=20, textvariable=eoutput)
-
-b1 = tk.Button(win, text="Yes",command=step1)
-b2 = tk.Button(win, text="No ",command=step2)
-b3 = tk.Button(win, text="Yes",command=step3)
-b4 = tk.Button(win, text="No ",command=step4)
-b5 = tk.Button(win, text="Calculate",command=calculate_1)
-b6 = tk.Button(win, text="Calculate",command=calculate_2)
-
+e6=tk.Entry(win,width=20, textvariable=eoutput)
 
 def step1():
     l1.grid(row=2,column=1)
@@ -46,9 +38,9 @@ def step1():
     b5.grid(row=3,column=1,columnspan=2)
 
 def step2():
-    l7.grid(row=2,column=1, columnspan=2)
-    b3.grid(row=2,column=2, columnspan=2)
-    b4.grid(row=2,column=3, columnspan=2)
+    l6.grid(row=2,column=1)
+    b3.grid(row=2,column=2)
+    b4.grid(row=2,column=3)
 
 def step3():
     l4.grid(row=3,column=1)
@@ -58,11 +50,15 @@ def step3():
     b6.grid(row=4,column=1,columnspan=2)    
 
 def step4():
+    l8.grid(row=5,column=3)
+    e6.grid(row=5,column=4)
     answer="cannot calculate"
-    e5.delete(0,END)
-    e5.insert(0,answer)
+    e6.delete(0,END)
+    e6.insert(0,answer)
 
 def calculate_1():
+    l8.grid(row=5,column=3)
+    e6.grid(row=5,column=4)
     answer=""
     num1 = e1.get()
     num2 = e2.get()
@@ -70,28 +66,37 @@ def calculate_1():
     num1=float(num1)
     num2=float(num2)
     num3=float(num3)
-    s=(num1+num2+num3)/2
+    s=float((num1+num2+num3)/2)
     c=math.sqrt(s*(s-num1)*(s-num2)*(s-num3))
     answer=str(c)
 
-    e5.delete(0,END)
-    e5.insert(0,answer)    
+    e6.delete(0,END)
+    e6.insert(0,answer)    
 
 def calculate_2():
+    l8.grid(row=5,column=3)
+    e6.grid(row=5,column=4)
     answer=""
     num1 = e4.get()
     num2 = e5.get()
     num1=float(num1)
     num2=float(num2)
-    c=(num1+num2)/2
+    c=(num1*num2)/2
     answer=str(c)
 
-    e5.delete(0,END)
-    e5.insert(0,answer)    
+    e6.delete(0,END)
+    e6.insert(0,answer)    
 
-l6.grid(row=1,column=1, columnspan=2)
-b1.grid(row=1,column=2, columnspan=2)
-b2.grid(row=1,column=3, columnspan=2)
+b1 = tk.Button(win, text="Yes",command=step1)
+b2 = tk.Button(win, text="No ",command=step2)
+b3 = tk.Button(win, text="Yes",command=step3)
+b4 = tk.Button(win, text="No ",command=step4)
+b5 = tk.Button(win, text="Calculate",command=calculate_1)
+b6 = tk.Button(win, text="Calculate",command=calculate_2)
+
+l7.grid(row=1,column=1)
+b1.grid(row=1,column=2)
+b2.grid(row=1,column=3)
 
 
 
